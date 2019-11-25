959aea79a04b0045c1819ec8462062b68f3aa8a5
Dan Barnes <dbarnes@robots.ox.ac.uk>
2019-11-25 12:40:40 +0000

    Implemented python based dataset downloader (pending OAuth verification for external users)

--- radar_robotcar_dataset_sdk/downloader/gdrive_handler.py
+++ radar_robotcar_dataset_sdk/downloader/gdrive_handler.py
@@ -13,137 +13,101 @@
 ###############################################################################
 
 from __future__ import division, print_function, absolute_import
-from absl import app, logging, flags
 import os.path
-import platform
-import subprocess
-from google_drive_downloader import GoogleDriveDownloader as gdd
-import sys
-import pexpect
+from absl import app, flags
+from tqdm import tqdm
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaIoBaseDownload
+import logging
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string("gdrive_binary_name", None, "GDrive binary name")
 
-# We use this forked gdrive which has Team Drive support
-# https://github.com/petrpulc/gdrive
-# Generate the below list with any gdrive instance with the command:
-# gdrive list  --query " '12GSQhLLdKDdKzq_a-7WOrip5HnFmm1v9' in parents"
-gdrive_binary_list_raw = """Id                                  Name                     Type   Size     Created
-1iIjBty1FKxdGvyc4GATngwgbzQdPYz2p   gdrive-linux-x64         bin    4.8 MB   2018-11-01 19:33:47
-1gXMutwGQ-HJ0zKwOKlMqEKt3197UhYn8   gdrive-linux-386         bin    4.2 MB   2018-11-01 19:32:59
-1LkAchi-D9KzvRcGWVcmSJl6c8PG9QWzS   gdrive-windows-x64.exe   bin    4.7 MB   2018-11-01 19:34:29
-1qBuNl46cSqYsbNBgNYAgH74NFhD11epX   gdrive-windows-386.exe   bin    4.2 MB   2018-11-01 19:34:26
-1pU9HVjiNvxaXyRtaX7hltyjyYF4glonT   gdrive-solaris-x64       bin    4.7 MB   2018-11-01 19:34:24
-13OZeLVUQJBC8WdMEtsKPR8eVVU1ZZ4AB   gdrive-plan9-x64         bin    4.5 MB   2018-11-01 19:34:22
-1smW7u9JANDGAZxQZjgWOj0Qr1Kj3h0P2   gdrive-plan9-386         bin    4.0 MB   2018-11-01 19:34:20
-1GL18Ety5Le8IpNwRKO1iZg1YWV_cCWUN   gdrive-osx-x64           bin    5.2 MB   2018-11-01 19:34:17
-1dGh5g88D2jq-KeVOMcbAYpJkd20mUBDi   gdrive-osx-386           bin    4.7 MB   2018-11-01 19:34:04
-1sLp4DRDKkyaKoUcz03UGjXR1zYdJb4-N   gdrive-openbsd-x64       bin    4.8 MB   2018-11-01 19:34:01
-1wlzUqpzsE9XS4rK2EVJ1C2WiWxL79QgM   gdrive-openbsd-arm       bin    4.4 MB   2018-11-01 19:33:59
-19CNffIDUygHWQWw6hP7tLECeqzfmaCwU   gdrive-openbsd-386       bin    4.2 MB   2018-11-01 19:33:57
-1xleqVF8HlkLp_el8-CS30g23Pta-IyUj   gdrive-netbsd-x64        bin    4.8 MB   2018-11-01 19:33:54
-14WbEoaHiPDNFi7dgecav6XrFAYZAYBME   gdrive-netbsd-arm        bin    4.4 MB   2018-11-01 19:33:52
-1Ev7sgDmYZoZb_krBd95fej_vaqYAIqtK   gdrive-netbsd-386        bin    4.2 MB   2018-11-01 19:33:49
-1SMrO7Kh3BjetpL2nBykgtjedVICZxJtJ   gdrive-linux-rpi         bin    4.4 MB   2018-11-01 19:33:45
-1Ul1NBy7S2fOEomF0pLY1RmmcLeYwsWFA   gdrive-linux-ppc64le     bin    5.0 MB   2018-11-01 19:33:43
-1kafdBT0nZllg2joivg_VQItTLD3mH_Cg   gdrive-linux-ppc64       bin    5.0 MB   2018-11-01 19:33:39
-1xazXLlduU1QSJYMU8Ch_WfIbkmrHGXct   gdrive-linux-mips64le    bin    5.7 MB   2018-11-01 19:33:37
-1sFHrFwVhR6su1iR39j5LR52loA9nA228   gdrive-linux-mips64      bin    5.7 MB   2018-11-01 19:33:34
-1N-26SYJ5IQ8hXbc_DTJ-tYMaoOpvmtKl   gdrive-linux-arm64       bin    4.9 MB   2018-11-01 19:33:31
-1enssEMj7FXEZUkbu8AVabOeKRmWt99wa   gdrive-linux-arm         bin    4.4 MB   2018-11-01 19:33:29
-1klUCs5B6uMJ-WoZFjq6S0kphM1eRAU0F   gdrive-freebsd-x64       bin    4.8 MB   2018-11-01 19:32:56
-1foJs2QP6Tv7EYyqvd5gnDbllxuwPn4aF   gdrive-freebsd-arm       bin    4.4 MB   2018-11-01 19:32:51
-1GXmTKyz4nXCSnX15CjmM7d3LzMBmBzdV   gdrive-freebsd-386       bin    4.2 MB   2018-11-01 19:32:49
-1zOWRZAnIHgaSXQpiqdirRc8q4gU8mdeg   gdrive-dragonfly-x64     bin    4.8 MB   2018-11-01 19:32:47"""
-gdrive_binary_list = [l.split() for l in gdrive_binary_list_raw.splitlines()[1:]]
-default_gdrive_dir = os.path.expanduser(os.path.join('~', '.radar_robotcar_dataset_downloader'))
+flags.DEFINE_float("download_chunk_size", 100, "Download chunk size in MB. Google API defaults to 100 MB.")
+flags.DEFINE_bool("http_server_auth", False,
+                  "Use the local server auth mode for the Google API. "
+                  "This is more intuitive but is not possible on remote servers and so is disabled by default")
+
+__CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+__SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+__CLIENT_SECRETS_FILE = os.path.join(__CURRENT_DIR, 'client_secrets.json')
+__GDRIVE_TOKEN_FILE = os.path.join(__CURRENT_DIR, "gdrive_token.pickle")
+
+# Disable verbose googleapiclient logging
+logging.getLogger("googleapiclient").setLevel(logging.WARNING)
 
 
 class GDriveHandler:
+    """A python googleapiclient Google Drive wrapper for downloading the Oxford Radar RobotCar Dataset"""
+
     def __init__(self, download_dir):
-        self.download_dir = download_dir
-        self.bin = check_gdrive_exists_or_download(self.download_dir, True)
 
-    def download(self, id, timeout=0):
-        args = [self.bin, 'download', "--timeout", str(timeout), "--force", "--path", self.download_dir, id]
-        res = subprocess.check_output(args)
-        output_file = res.decode().splitlines()[0].split()[-1]
-        return output_file
+        self.service = _get_drive_service()
+        self.download_dir = os.path.expanduser(download_dir)
+        # This access doesn't fail if FLAGS haven't been parsed
+        self.download_chunk_size_bytes = int(FLAGS['download_chunk_size'].value * 1024 * 1024)
 
+        if not os.path.isdir(self.download_dir):
+            os.makedirs(self.download_dir)
+            if not os.path.isdir(self.download_dir):
+                raise NotADirectoryError(f'Could not create download directory: {self.download_dir}')
 
-def check_gdrive_exists_or_download(download_gdrive_dir, check_authorised=True):
-    # This is not very robust but covers the main systems
+    def download(self, file_id):
 
-    if (not FLAGS.is_parsed()) or (FLAGS.gdrive_binary_name is None):
-        system = platform.system().lower()
-        if system == "darwin":
-            system = "osx"
-        processor_string_lookup = {
-            "i386": "386",
-            'x86_64': "x64",
-        }
-        processor_str = processor_string_lookup[platform.processor()]
-        gdrive_binary_name_search = "-".join(["gdrive", system, processor_str])
-    else:
-        gdrive_binary_name_search = FLAGS.gdrive_binary_name
+        # Set up download of file with fileID = <file_id>
+        request = self.service.files().get_media(fileId=file_id, supportsTeamDrives=True)
+        meta = self.service.files().get(fileId=file_id, supportsTeamDrives=True,
+                                        fields='originalFilename, size').execute()
+        filename = meta['originalFilename']
+        output_path = os.path.join(self.download_dir, filename)
+        with open(output_path, 'wb') as output_file:
+            downloader = MediaIoBaseDownload(output_file, request, chunksize=self.download_chunk_size_bytes)
 
-    if system == "linux":
-        if download_gdrive_dir[:4] == "/tmp":
-            raise RuntimeError(f'Some linux distros cannot run executables in `/tmp`. '
-                               f'Please change you download directory from: {download_gdrive_dir}')
+            # Perform download and stream data into output_file
+            done = False
+            desc = f"Downloading FileID {file_id} - {filename}"
+            with tqdm(total=int(meta['size']), unit_scale=True, unit_divisor=1024, desc=desc) as pbar:
+                while done is False:
+                    status, done = downloader.next_chunk()
+                    pbar.update(status.resumable_progress - pbar.n)
 
-    try:
-        candidate_binaries = [g for g in gdrive_binary_list if g[1] == gdrive_binary_name_search]
-        gdrive_binary = candidate_binaries[0]
-        gdrive_binary_name = gdrive_binary[1]
-    except KeyError or IndexError:
-        print(f"Could not automatically determine the correct gdrive binary string for system: {platform.platform()}")
-        print(f"Please feed the correct Name of the binaries from below as flag --gdrive_binary_name")
-        print(gdrive_binary_list_raw)
-        raise LookupError
-
-    # Try and find existing gdrive exectutable first
-    gdrive_binary_path = f'{download_gdrive_dir}/{gdrive_binary_name}'
-    found = os.path.isfile(gdrive_binary_path)
-    if not found:
-        gdrive_binary_path = f"/{download_gdrive_dir}/{gdrive_binary[1]}"
-        gdd.download_file_from_google_drive(file_id=gdrive_binary[0],
-                                            dest_path=gdrive_binary_path)
-        subprocess.check_output(["chmod", "+x", gdrive_binary_path])
-
-    print(f"Using GDrive binary: {gdrive_binary_path}")
-    sys.stdout.flush()
-    if check_authorised:
-        verify_gdrive_authorised(gdrive_binary_path)
-    return gdrive_binary_path
+        return output_path
 
 
-def verify_gdrive_authorised(gdrive_binary_path):
-    verification_code = "Enter verification code:"
-    no_verification_code = "Max upload size:"
-    child = pexpect.spawn(gdrive_binary_path, ["about"])
-    returns = [verification_code, no_verification_code, pexpect.EOF]
-    index = child.expect(returns)
-    if returns[index] == pexpect.EOF:
-        raise RuntimeError(f'Unexpected GDrive branch')
-    elif returns[index] == no_verification_code:
-        return
-    else:
-        print("\n" + child.before.decode())
-        print(child.after.decode())
-        child.sendline(input())
-        returns = [no_verification_code, pexpect.EOF]
-        index = child.expect(returns)
-        if returns[index] == pexpect.EOF:
-            raise RuntimeError(f'Unexpected GDrive branch')
-        elif returns[index] == no_verification_code:
-            print("\nGDrive Authorised\n")
-            return
+def _get_drive_service():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(__GDRIVE_TOKEN_FILE):
+        with open(__GDRIVE_TOKEN_FILE, 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                __CLIENT_SECRETS_FILE, __SCOPES)
+            if FLAGS['http_server_auth'].value:
+                # Run local server based authentication
+                creds = flow.run_local_server()
+            else:
+                # Run console based authentication
+                creds = flow.run_console()
+        # Save the credentials for the next run
+        with open(__GDRIVE_TOKEN_FILE, 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds, cache_discovery=False)
+    return service
 
 
 def main(unused_args):
-    check_gdrive_exists_or_download(True)
-    gdrive = GDriveHandler()
+    gdrive = GDriveHandler(download_dir='/tmp/radar_robotcar_dataset_test_download')
 
 
 if __name__ == '__main__':
