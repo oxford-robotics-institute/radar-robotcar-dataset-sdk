055d7edad240a57c3538dcf1cff696b893b8fec1
Dan Barnes <dbarnes@robots.ox.ac.uk>
2020-02-05 10:11:41 +0000

    Untested possible fix for downloading on Windows machines

--- radar_robotcar_dataset_sdk/downloader/gdrive_handler.py
+++ radar_robotcar_dataset_sdk/downloader/gdrive_handler.py
@@ -17,15 +17,20 @@ import os.path
 import subprocess
 import os
 import shutil
+import platform
+import requests
+from tempfile import NamedTemporaryFile
+from zipfile import ZipFile
 
 _ls = "=" * 100  # Logging separator
 _current_dir = os.path.expanduser(os.path.abspath(os.path.dirname(__file__)))
 _rrcd_registration_url = "https://ori.ox.ac.uk/datasets/radar-robotcar-dataset/registration"
+_isWindows = platform.system() == "Windows"
 
 _rclone_version = "v1.50.1"
 _rclone_install_script_path = os.path.join(_current_dir, 'rclone', "install.sh")
 _rclone_patched_install_script_name = "patched_rclone_install.sh"
-_rclone_binary_name = "rclone"
+_rclone_binary_name = "rclone.exe" if _isWindows else "rclone"
 _rclone_rrcd_conf_unauthorised_name = "rclone_rrcd_unauthorised.conf"
 _rclone_rrcd_conf_authorised_name = "rclone_rrcd.conf"
 _rclone_rrcd_conf_drive_path = "rrcd_drive:"
@@ -91,22 +96,49 @@ def _initialise_dir_with_rclone(install_dir, create_dir_if_needed=True):
     # Run modified rclone install script with download folder and rclone version
     ##########################################################################################
     print("{}\nInitialising rclone to: {}\n{}".format(_ls, install_dir, _ls))
-    return_code = subprocess.call(['bash', _rclone_install_script_path, install_dir, _rclone_version],
-                                  stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    if return_code == 0:
-        print('rclone install script: exited without problems')
-    elif return_code == 1:
-        raise RuntimeError(
-            'rclone install script: parameters not supported were used or some unexpected error occurred')
-    elif return_code == 2:
-        raise RuntimeError('rclone install script: OS not supported by this script')
-    elif return_code == 3:
-        print('rclone install script: installed version of rclone is already at requested version: '
-              '{}'.format(_rclone_version))
-    elif return_code == 4:
-        raise RuntimeError('rclone install script: supported unzip tools are not available')
+
+    if not _isWindows:
+        return_code = subprocess.call(['bash', _rclone_install_script_path, install_dir, _rclone_version],
+                                      stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        if return_code == 0:
+            print('rclone install script: exited without problems')
+        elif return_code == 1:
+            raise RuntimeError(
+                'rclone install script: parameters not supported were used or some unexpected error occurred')
+        elif return_code == 2:
+            raise RuntimeError('rclone install script: OS not supported by this script')
+        elif return_code == 3:
+            print('rclone install script: installed version of rclone is already at requested version: '
+                  '{}'.format(_rclone_version))
+        elif return_code == 4:
+            raise RuntimeError('rclone install script: supported unzip tools are not available')
+        else:
+            raise RuntimeError('rclone install script: unexpected return code {}'.format(return_code))
     else:
-        raise RuntimeError('rclone install script: unexpected return code {}'.format(return_code))
+
+        # If rclone exists, check the version is correct
+        download_rclone_path = os.path.join(install_dir, _rclone_binary_name)
+        rclone_exists = os.path.isfile(download_rclone_path)
+        if rclone_exists:
+            res = subprocess.check_output([download_rclone_path, "version"]).decode('utf-8').splitlines()[0]
+            correct_version = _rclone_version in res
+
+        if (not rclone_exists) or (not correct_version):
+            windows_binary_download_url = \
+                "https://downloads.rclone.org/{}/rclone-{}-windows-amd64.zip".format(_rclone_version, _rclone_version)
+            print("Downloading rclone (Windows):\nSource: {}\nDest  : {}".format(windows_binary_download_url,
+                                                                                 download_rclone_path))
+            # Download and unzip
+            rclone_versioned_download_folder = os.path.splitext(os.path.split(windows_binary_download_url)[1])[0]
+            response = requests.get(windows_binary_download_url)
+            with NamedTemporaryFile('wb') as tmpfile:
+                tmpfile.file.write(response.content)
+                ZipFile(tmpfile.name).extractall(install_dir)
+
+            # Copy to correct location (leave original download for debugging)
+            rclone_bin_copy_src = os.path.join(install_dir, rclone_versioned_download_folder, _rclone_binary_name)
+            rclone_bin_copy_dest = os.path.join(install_dir, _rclone_binary_name)
+            shutil.copyfile(rclone_bin_copy_src, rclone_bin_copy_dest)
 
     ##########################################################################################
     # Check the rclone binary is found as expected
@@ -139,5 +171,5 @@ def _initialise_dir_with_rclone(install_dir, create_dir_if_needed=True):
 
 
 if __name__ == '__main__':
-    gdrive = GDriveHandler(download_dir='/tmp/radar_robotcar_dataset_test_download3')
+    gdrive = GDriveHandler(download_dir='/tmp/radar_robotcar_dataset_test_download')
     gdrive.download_filename("2019-01-15-14-24-38-radar-oxford-10k_Bumblebee_XB3_Visual_Odometry")
